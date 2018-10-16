54c18509ea031d0fd0c1216db3f84169e438434f
Vadim <vsavdev@gmail.com>
2018-10-16 10:00:33 +0300

    add 1-to-many article-comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_124023) do
+ActiveRecord::Schema.define(version: 2018_10_16_065231) do
 
   create_table "abouts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2018_10_15_124023) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.text "body"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "contacts", force: :cascade do |t|
