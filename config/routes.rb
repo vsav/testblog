5210e708b64ad083115d0e5171b6d3ad0ef11f3c
Vadim <vsavdev@gmail.com>
2018-10-16 12:28:49 +0300

    show comments on article page

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
 
-  resources :articles
   resource :contacts, only: [:new, :create], path_names: { :new => ''} #чтобы вид new выводился по ссылке /contacts, а не /contacts/new
+  resources :articles do
+    resources :comments
+  end
 end
 
 
