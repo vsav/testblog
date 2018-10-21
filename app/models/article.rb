4cafa28ee0511d852c18ceb4800e3c6e05e5f435
Vadim <vsavdev@gmail.com>
2018-10-21 16:20:11 +0300

    add article model validation tests

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,6 @@
 class Article < ApplicationRecord
-=begin
+
   validates :title, presence: true
-=end
   validates :text, presence: true
   has_many :comments, :dependent => :destroy
   belongs_to :user, :class_name => 'User', :foreign_key => 'username', optional: true
